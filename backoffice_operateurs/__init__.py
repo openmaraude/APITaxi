2b26607e36cd7e3812c4504df5b25f5df3fb855a
l-vincent-l <lara.vincent@gmail.com>
2015-04-02 16:27:24 +0200

    Activate authentication without form

--- backoffice_operateurs/__init__.py
+++ backoffice_operateurs/__init__.py
@@ -9,6 +9,7 @@ __version__ = ".".join(map(str, VERSION))
 from flask import Flask
 from flask.ext.security import Security, SQLAlchemyUserDatastore
 from flask.ext.script import Manager
+from flask.ext.security.utils import verify_and_update_password
 from flask_bootstrap import Bootstrap
 import os
 from models import db, security as security_models, taxis as taxis_models,\
@@ -24,7 +25,6 @@ db.init_app(app)
 user_datastore = SQLAlchemyUserDatastore(db, security_models.User,
                             security_models.Role)
 security = Security(app, user_datastore)
-
 from views import ads
 from views import conducteur
 from views import zupc
@@ -33,7 +33,29 @@ from views import home
 app.register_blueprint(ads.mod)
 app.register_blueprint(conducteur.mod)
 app.register_blueprint(zupc.mod)
-app.register_blueprint(home.mod)
+app.register_blueprint(home.
+
+@app.login_manager.request_loader
+def load_user_from_request(request):
+    auth = request.headers.get('Authorization')
+    if not auth:
+        return None
+    auth_splitted = auth.split(':')
+    if len(auth_splitted) != 2:
+        return None
+    login, password = auth_splitted
+    user = user_datastore.get_user(login.strip())
+    if user is None:
+        return None
+    if not verify_and_update_password(password.strip(), user):
+        return None
+    if not user.is_active():
+        return None
+    return user
+
+
+
+
 
 Bootstrap(app)
 
---
+++ backoffice_operateurs/utils/asdict_mixin.py
@@ -0,0 +1,3 @@
+class AsDictMixin:
+    def as_dict(self):
+       return {c.name: getattr(self, c.name) for c in self.__table__.columns}
