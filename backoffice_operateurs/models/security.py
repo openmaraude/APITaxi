2c99820ef80b215d99144ac3097cbc076f67a8a8
l-vincent-l <lara.vincent@gmail.com>
2015-04-02 10:32:16 +0200

    Add column

--- backoffice_operateurs/models/security.py
+++ backoffice_operateurs/models/security.py
@@ -18,3 +18,4 @@ class User(db.Model, UserMixin):
     confirmed_at = db.Column(db.DateTime())
     roles = db.relationship('Role', secondary=roles_users,
                             backref=db.backref('users', lazy='dynamic'))
+    auth_token = db.Column(db.String(255), nullable=False)
