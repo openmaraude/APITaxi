a5b0253ea53440b9cb6752d503411ea573f6ddce
l-vincent-l <lara.vincent@gmail.com>
2015-04-02 16:29:36 +0200

    Remove auth token

--- backoffice_operateurs/models/security.py
+++ backoffice_operateurs/models/security.py
@@ -18,4 +18,3 @@ class User(db.Model, UserMixin):
     confirmed_at = db.Column(db.DateTime())
     roles = db.relationship('Role', secondary=roles_users,
                             backref=db.backref('users', lazy='dynamic'))
-    auth_token = db.Column(db.String(255), nullable=False)
