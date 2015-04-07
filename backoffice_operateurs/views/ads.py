l-vincent-l/backoffice_operateurs

Refactorize ADS forms

backoffice_operateurs/views/ads.py
@@ -10,67 +10,14 @@ from wtforms import StringField
 from datetime import datetime
 
 
-@app.route('/ads')
-@app.route('/ads/')
-@login_required
+
+
 def ads_list():
     page = int(request.args.get('page')) if 'page' in request.args else 1
     return render_template('lists/ads.html',
         ads_list=taxis_models.ADS.query.paginate(page))
 
-
-@app.route('/ads/create', methods=['GET', 'POST'])
-@login_required
-def ads_create():
-    form = taxis_forms.ADSCreateForm()
-    if request.method == "POST" and form.validate():
-        ads = taxis_models.ADS()
-        ads.added_at = datetime.now().isoformat()
-        ads.added_by = current_user.id
-        ads.added_via = "form"
-        ads.source = "user"
-        form.populate_obj(ads)
-        db.session.add(ads)
-        db.session.commit()
-        return redirect(url_for('ads_list'))
-    return render_template('forms/ads.html', form=form, form_method="POST")
-
-
-@app.route('/ads/update', methods=['GET', 'POST'])
-@login_required
-def ads_update():
-    if not request.args.get("id"):
-        abort(404)
-    ads = taxis_models.ADS.query.get(request.args.get("id"))
-    if not ads:
-        abort(404)
-    form = taxis_forms.ADSUpdateForm(obj=ads, zupc=ads.ZUPC.nom)
-    if request.method == "POST":
-        ads.last_update_at = datetime.now().isoformat()
-        form.populate_obj(ads)
-        if form.validate():
-            db.session.commit()
-            return redirect(url_for('ads_list'))
-
-    return render_template('forms/ads.html', form=form,
-        form_method="POST")
-
-
-@app.route('/ads/delete')
-@login_required
-def ads_delete():
-    if not request.args.get("id"):
-        abort(404)
-    ads = taxis_models.ADS.query.get(request.args.get("id"))
-    if not ads:
-        abort(404)
-    db.session.delete(ads)
-    db.session.commit()
-    return redirect(url_for("ads_list"))
-
-@app.route('/ads_create', methods=['POST'])
-@login_required
-def ads_api_add():
+def ads_create_api():
     json = request.get_json()
     if "ads" not in json:
         abort(400)
@@ -83,3 +30,51 @@ def ads_api_add():
     db.session.add(new_ads)
     db.session.commit()
     return jsonify(new_ads.as_dict())
+
+@app.route('/ads', methods=['GET', 'POST'])
+@login_required
+def ads():
+    if request.method == 'GET':
+        return ads_list()
+    elif request.method == 'POST':
+        return ads_create_api()
+
+
+
+
+@app.route('/ads/form', methods=['GET', 'POST'])
+@login_required
+def ads_form():
+    ads = zupc = form = None
+    if request.args.get("id"):
+        ads = taxis_models.ADS.query.get(request.args.get("id"))
+        if not ads:
+            abort(404)
+        form = ADSUpdateForm(obj=ads, zupc=ads.ZUPC.nom)
+    else:
+        form = ADSCreateForm()
+    if request.method == "POST":
+        if not ads and form.validate():
+            ads = taxis_models.ADS()
+            form.populate_obj(ads)
+            db.session.add(ads)
+            db.session.commit()
+            return redirect(url_for('ads'))
+        elif ads:
+            form.populate_obj(ads)
+            if form.validate():
+                db.session.commit()
+                return redirect(url_for('ads'))
+    return render_template('forms/ads.html', form=form)
+
+@app.route('/ads/delete')
+@login_required
+def ads_delete():
+    if not request.args.get("id"):
+        abort(404)
+    ads = taxis_models.ADS.query.get(request.args.get("id"))
+    if not ads:
+        abort(404)
+    db.session.delete(ads)
+    db.session.commit()
+    return redirect(url_for("ads"))

openmaraude/APITaxi

backoffice_operateurs/views/ads.py
@@ -11,67 +11,12 @@ from datetime import datetime
 
 mod = Blueprint('ads', __name__)
 
-@mod.route('/ads')
-@mod.route('/ads/')
-@login_required
 def ads_list():
     page = int(request.args.get('page')) if 'page' in request.args else 1
     return render_template('lists/ads.html',
         ads_list=taxis_models.ADS.query.paginate(page))
 
-
-@mod.route('/ads/create', methods=['GET', 'POST'])
-@login_required
-def ads_create():
-    form = taxis_forms.ADSCreateForm()
-    if request.method == "POST" and form.validate():
-        ads = taxis_models.ADS()
-        ads.added_at = datetime.now().isoformat()
-        ads.added_by = current_user.id
-        ads.added_via = "form"
-        ads.source = "user"
-        form.populate_obj(ads)
-        db.session.add(ads)
-        db.session.commit()
-        return redirect(url_for('ads_list'))
-    return render_template('forms/ads.html', form=form, form_method="POST")
-
-
-@mod.route('/ads/update', methods=['GET', 'POST'])
-@login_required
-def ads_update():
-    if not request.args.get("id"):
-        abort(404)
-    ads = taxis_models.ADS.query.get(request.args.get("id"))
-    if not ads:
-        abort(404)
-    form = taxis_forms.ADSUpdateForm(obj=ads, zupc=ads.ZUPC.nom)
-    if request.method == "POST":
-        ads.last_update_at = datetime.now().isoformat()
-        form.populate_obj(ads)
-        if form.validate():
-            db.session.commit()
-            return redirect(url_for('ads_list'))
-
-    return render_template('forms/ads.html', form=form,
-        form_method="POST")
-
-
-@mod.route('/ads/delete')
-@login_required
-def ads_delete():
-    if not request.args.get("id"):
-        abort(404)
-    ads = taxis_models.ADS.query.get(request.args.get("id"))
-    if not ads:
-        abort(404)
-    db.session.delete(ads)
-    db.session.commit()
-    return redirect(url_for("ads_list"))
-
-@mod.route('/ads_create', methods=['POST'])
-@login_required
-def ads_api_add():
+def ads_create_api():
     json = request.get_json()
     if "ads" not in json:
         abort(400)
@@ -84,3 +29,49 @@ def ads_api_add():
     db.session.add(new_ads)
     db.session.commit()
     return jsonify(new_ads.as_dict())
+
+@mod.route('/ads', methods=['GET', 'POST'])
+@login_required
+def ads():
+    if request.method == 'GET':
+        return ads_list()
+    elif request.method == 'POST':
+        return ads_create_api()
+
+
+@mod.route('/ads/form', methods=['GET', 'POST'])
+@login_required
+def ads_form():
+    ads = zupc = form = None
+    if request.args.get("id"):
+        ads = taxis_models.ADS.query.get(request.args.get("id"))
+        if not ads:
+            abort(404)
+        form = ADSUpdateForm(obj=ads, zupc=ads.ZUPC.nom)
+    else:
+        form = ADSCreateForm()
+    if request.method == "POST":
+        if not ads and form.validate():
+            ads = taxis_models.ADS()
+            form.populate_obj(ads)
+            db.session.add(ads)
+            db.session.commit()
+            return redirect(url_for('ads'))
+        elif ads:
+            form.populate_obj(ads)
+            if form.validate():
+                db.session.commit()
+                return redirect(url_for('ads'))
+    return render_template('forms/ads.html', form=form)
+
+@mod.route('/ads/delete')
+@login_required
+def ads_delete():
+    if not request.args.get("id"):
+        abort(404)
+    ads = taxis_models.ADS.query.get(request.args.get("id"))
+    if not ads:
+        abort(404)
+    db.session.delete(ads)
+    db.session.commit()
+    return redirect(url_for("ads"))