l-vincent-l/backoffice_operateurs

refactorization conducteurs and ZUPC

backoffice_operateurs/views/conducteur.py
@@ -1,70 +1,11 @@
 from backoffice_operateurs import app, db
-from backoffice_operateurs.forms import taxis as taxis_forms
+from backoffice_operateurs.forms.taxis import ConducteurCreateForm,\
+        ConducteurUpdateForm
 from backoffice_operateurs.models import taxis as taxis_models
 from flask import render_template, request, redirect, url_for, abort, jsonify
 from flask.ext.security import login_required
 from backoffice_operateurs.utils import create_obj_from_json
 
-@app.route('/conducteurs')
-@app.route('/conducteurs/')
-@login_required
-def conducteurs_list():
-    page = int(request.args.get('page')) if 'page' in request.args else 1
-    return render_template('lists/conducteurs.html',
-        conducteur_list=taxis_models.Conducteur.query.paginate(page))
-
-
-@app.route('/conducteur/create', methods=['GET', 'POST'])
-def conducteur_create():
-    form = taxis_forms.ConducteurCreateForm()
-    if request.method == "POST" and form.validate():
-        conducteur = taxis_models.Conducteur()
-        conducteur.added_at = datetime.now().isoformat()
-        conducteur.added_by = current_user.id
-        conducteur.added_via = "form"
-        conducteur.source = "user"
-        form.populate_obj(conducteur)
-        db.session.add(conducteur)
-        db.session.commit()
-        return redirect(url_for('conducteurs_list'))
-    return render_template('forms/conducteur.html', form=form,
-        form_method="POST", submit_value="Creer")
-
-@app.route('/conducteur/update', methods=['GET', 'POST'])
-@login_required
-def conducteur_update():
-    if not request.args.get("id"):
-        abort(404)
-    conducteur = taxis_models.Conducteur.query.get(request.args.get("id"))
-    if not conducteur:
-        abort(404)
-    form = taxis_forms.ConducteurUpdateForm(obj=conducteur)
-    if request.method == "POST":
-        form.populate_obj(conducteur)
-        conducteur.last_update_at =  datetime.now().isoformat()
-        if form.validate():
-            db.session.commit()
-            return redirect(url_for('conducteurs_list'))
-
-    return render_template('forms/conducteur.html', form=form,
-        form_method="POST", submit_value="Modifier")
-
-
-@app.route('/conducteur/delete')
-@login_required
-def conducteur_delete():
-    if not request.args.get("id"):
-        abort(404)
-    conducteur = taxis_models.Conducteur.query.get(request.args.get("id"))
-    if not conducteur:
-        abort(404)
-    db.session.delete(conducteur)
-    db.session.commit()
-    return redirect(url_for("conducteurs_list"))
-
-
-@app.route('/conducteur', methods=['POST'])
-@login_required
 def conducteur_api_add():
     json = request.get_json()
     if "conducteur" not in json:
@@ -80,3 +21,58 @@ def conducteur_api_add():
     db.session.commit()
     return jsonify(new_conducteur.as_dict())
 
+
+def conducteurs_list():
+    page = int(request.args.get('page')) if 'page' in request.args else 1
+    return render_template('lists/conducteurs.html',
+        conducteur_list=taxis_models.Conducteur.query.paginate(page))
+
+
+@app.route('/conducteurs', methods=['GET', 'POST'])
+@app.route('/conducteurs/', methods=['GET', 'POST'])
+@login_required
+def conducteurs():
+    if request.method == 'GET':
+        return conducteurs_list()
+    elif request.method == 'POST':
+        return conducteur_api_add()
+    abort(405)
+
+@app.route('/conducteur/form', methods=['GET', 'POST'])
+@login_required
+def conducteur_form():
+    form = None
+    if request.args.get("id"):
+        conducteur = taxis_models.Conducteur.query.get(request.args.get("id"))
+        if not conducteur:
+            abort(404)
+        form = ConducteurUpdateForm(obj=conducteur)
+    else:
+        form = ConducteurCreateForm()
+    if request.method == "POST":
+        if request.args.get("id"):
+            form.populate_obj(conducteur)
+            if form.validate():
+                db.session.commit()
+                return redirect(url_for('conducteurs'))
+        else:
+            conducteur = taxis_models.Conducteur()
+            form.populate_obj(conducteur)
+            db.session.add(conducteur)
+            db.session.commit()
+            return redirect(url_for('conducteurs'))
+    return render_template('forms/conducteur.html', form=form,
+        form_method="POST", submit_value="Modifier")
+
+
+@app.route('/conducteur/delete')
+@login_required
+def conducteur_delete():
+    if not request.args.get("id"):
+        abort(404)
+    conducteur = taxis_models.Conducteur.query.get(request.args.get("id"))
+    if not conducteur:
+        abort(404)
+    db.session.delete(conducteur)
+    db.session.commit()
+    return redirect(url_for("conducteurs"))

backoffice_operateurs/views/zupc.py
@@ -1,46 +1,51 @@
 # -*- coding: utf8 -*-
 from backoffice_operateurs import app, db
 from backoffice_operateurs.models import administrative as administrative_models
-from backoffice_operateurs.forms import administrative as administrative_forms
+from backoffice_operateurs.forms.administrative import ZUPCreateForm, ZUPCUpdateForm
 from flask.ext.security import login_required
 from flask import request, render_template, redirect, jsonify, url_for
 
-@app.route('/zupc')
-@app.route('/zupc/')
-@login_required
+
 def zupc_list():
     page = int(request.args.get('page')) if 'page' in request.args else 1
     return render_template('lists/zupc.html',
         zupc_list=administrative_models.ZUPC.query.paginate(page))
 
 
-@app.route('/zupc/create', methods=['GET', 'POST'])
+@app.route('/zupc')
 @login_required
-def zupc_create():
-    form = administrative_forms.ZUPCreateForm()
-    if request.method == "POST" and form.validate():
-        zupc = administrative_models.ZUPC()
-        form.populate_obj(zupc)
-        db.session.add(zupc)
-        db.session.commit()
-        return redirect(url_for('zupc_list'))
-    return render_template('forms/zupc.html', form=form, form_method="POST")
+def zupc():
+    if request.method == "GET":
+        return zupc_list()
+    #elif request.method == "POST":
+    #    return zupc_create()
+    abort(405)
 
-@app.route('/zupc/update', methods=['GET', 'POST'])
+
+@app.route('/zupc/form', methods=['GET', 'POST'])
 @login_required
 def zupc_update():
-    if not request.args.get("id"):
-        abort(404)
-    zupc = administrative_models.ZUPC.query.get(request.args.get("id"))
-    if not zupc:
-        abort(404)
-    form = administrative_forms.ZUPCUpdateForm(form_data={"zupc":ads.ZUPC.nom}, obj=ads)
+    form = None
+    if request.args.get("id"):
+        zupc = administrative_models.ZUPC.query.get(request.args.get("id"))
+        if not zupc:
+            abort(404)
+        form = ZUPCUpdateForm(obj=zupc)
+    else:
+        form = ZUPCreateForm()
     if request.method == "POST":
-        form.populate_obj(zupc)
-        if form.validate():
-            db.session.commit()
-            return redirect(url_for('zupc_list'))
-
+        if request.args.get("id"):
+            form.populate_obj(zupc)
+            if form.validate():
+                db.session.commit()
+                return redirect(url_for('zupc'))
+        else:
+            if form.validate():
+                zupc = administrative_models.ZUPC()
+                form.populate_obj(zupc)
+                db.session.add(zupc)
+                db.session.commit()
+                return redirect(url_for('zupc'))
     return render_template('forms/ads.html', form=form,
         form_method="POST", submit_value="Modifier")
 
@@ -55,7 +60,7 @@ def zupc_delete():
         abort(404)
     db.session.delete(zupc)
     db.session.commit()
-    return redirect(url_for("zupc_list"))
+    return redirect(url_for("zupc"))
 
 
 @app.route('/zupc/autocomplete')

openmaraude/APITaxi

backoffice_operateurs/views/conducteur.py
@@ -1,6 +1,7 @@
 # -*- coding: utf8 -*-
 from backoffice_operateurs import db
-from backoffice_operateurs.forms import taxis as taxis_forms
+from backoffice_operateurs.forms.taxis import ConducteurCreateForm,\
+        ConducteurUpdateForm
 from backoffice_operateurs.models import taxis as taxis_models
 from flask import Blueprint, render_template, request, redirect, url_for, abort
 from flask import render_template, request, redirect, url_for, abort, jsonify
@@ -9,66 +10,6 @@ from backoffice_operateurs.utils import create_obj_from_json
 
 mod = Blueprint('conducteur', __name__)
 
-@mod.route('/conducteurs')
-@mod.route('/conducteurs/')
-@login_required
-def conducteurs_list():
-    page = int(request.args.get('page')) if 'page' in request.args else 1
-    return render_template('lists/conducteurs.html',
-        conducteur_list=taxis_models.Conducteur.query.paginate(page))
-
-
-@mod.route('/conducteur/create', methods=['GET', 'POST'])
-def conducteur_create():
-    form = taxis_forms.ConducteurCreateForm()
-    if request.method == "POST" and form.validate():
-        conducteur = taxis_models.Conducteur()
-        conducteur.added_at = datetime.now().isoformat()
-        conducteur.added_by = current_user.id
-        conducteur.added_via = "form"
-        conducteur.source = "user"
-        form.populate_obj(conducteur)
-        db.session.add(conducteur)
-        db.session.commit()
-        return redirect(url_for('conducteurs_list'))
-    return render_template('forms/conducteur.html', form=form,
-        form_method="POST", submit_value="Creer")
-
-@mod.route('/conducteur/update', methods=['GET', 'POST'])
-@login_required
-def conducteur_update():
-    if not request.args.get("id"):
-        abort(404)
-    conducteur = taxis_models.Conducteur.query.get(request.args.get("id"))
-    if not conducteur:
-        abort(404)
-    form = taxis_forms.ConducteurUpdateForm(obj=conducteur)
-    if request.method == "POST":
-        form.populate_obj(conducteur)
-        conducteur.last_update_at =  datetime.now().isoformat()
-        if form.validate():
-            db.session.commit()
-            return redirect(url_for('conducteurs_list'))
-
-    return render_template('forms/conducteur.html', form=form,
-        form_method="POST", submit_value="Modifier")
-
-
-@mod.route('/conducteur/delete')
-@login_required
-def conducteur_delete():
-    if not request.args.get("id"):
-        abort(404)
-    conducteur = taxis_models.Conducteur.query.get(request.args.get("id"))
-    if not conducteur:
-        abort(404)
-    db.session.delete(conducteur)
-    db.session.commit()
-    return redirect(url_for("conducteurs_list"))
-
-
-@mod.route('/conducteur', methods=['POST'])
-@login_required
 def conducteur_api_add():
     json = request.get_json()
     if "conducteur" not in json:
@@ -84,3 +25,58 @@ def conducteur_api_add():
     db.session.commit()
     return jsonify(new_conducteur.as_dict())
 
+
+def conducteurs_list():
+    page = int(request.args.get('page')) if 'page' in request.args else 1
+    return render_template('lists/conducteurs.html',
+        conducteur_list=taxis_models.Conducteur.query.paginate(page))
+
+
+@mod.route('/conducteurs', methods=['GET', 'POST'])
+@mod.route('/conducteurs/', methods=['GET', 'POST'])
+@login_required
+def conducteurs():
+    if request.method == 'GET':
+        return conducteurs_list()
+    elif request.method == 'POST':
+        return conducteur_api_add()
+    abort(405)
+
+@mod.route('/conducteur/form', methods=['GET', 'POST'])
+@login_required
+def conducteur_form():
+    form = None
+    if request.args.get("id"):
+        conducteur = taxis_models.Conducteur.query.get(request.args.get("id"))
+        if not conducteur:
+            abort(404)
+        form = ConducteurUpdateForm(obj=conducteur)
+    else:
+        form = ConducteurCreateForm()
+    if request.method == "POST":
+        if request.args.get("id"):
+            form.populate_obj(conducteur)
+            if form.validate():
+                db.session.commit()
+                return redirect(url_for('conducteurs'))
+        else:
+            conducteur = taxis_models.Conducteur()
+            form.populate_obj(conducteur)
+            db.session.add(conducteur)
+            db.session.commit()
+            return redirect(url_for('conducteurs'))
+    return render_template('forms/conducteur.html', form=form,
+        form_method="POST", submit_value="Modifier")
+
+
+@mod.route('/conducteur/delete')
+@login_required
+def conducteur_delete():
+    if not request.args.get("id"):
+        abort(404)
+    conducteur = taxis_models.Conducteur.query.get(request.args.get("id"))
+    if not conducteur:
+        abort(404)
+    db.session.delete(conducteur)
+    db.session.commit()
+    return redirect(url_for("conducteurs"))

backoffice_operateurs/views/zupc.py
@@ -1,48 +1,53 @@
 # -*- coding: utf8 -*-
 from backoffice_operateurs import db
 from backoffice_operateurs.models import administrative as administrative_models
-from backoffice_operateurs.forms import administrative as administrative_forms
+from backoffice_operateurs.forms.administrative import ZUPCreateForm, ZUPCUpdateForm
 from flask.ext.security import login_required
 from flask import Blueprint, request, render_template, redirect, jsonify, url_for
 
+
 mod = Blueprint('zupc', __name__)
 
-@mod.route('/zupc')
-@mod.route('/zupc/')
-@login_required
 def zupc_list():
     page = int(request.args.get('page')) if 'page' in request.args else 1
     return render_template('lists/zupc.html',
         zupc_list=administrative_models.ZUPC.query.paginate(page))
 
 
-@mod.route('/zupc/create', methods=['GET', 'POST'])
+@mod.route('/zupc')
 @login_required
-def zupc_create():
-    form = administrative_forms.ZUPCreateForm()
-    if request.method == "POST" and form.validate():
-        zupc = administrative_models.ZUPC()
-        form.populate_obj(zupc)
-        db.session.add(zupc)
-        db.session.commit()
-        return redirect(url_for('zupc_list'))
-    return render_template('forms/zupc.html', form=form, form_method="POST")
+def zupc():
+    if request.method == "GET":
+        return zupc_list()
+    #elif request.method == "POST":
+    #    return zupc_create()
+    abort(405)
 
-@mod.route('/zupc/update', methods=['GET', 'POST'])
+
+@mod.route('/zupc/form', methods=['GET', 'POST'])
 @login_required
 def zupc_update():
-    if not request.args.get("id"):
-        abort(404)
-    zupc = administrative_models.ZUPC.query.get(request.args.get("id"))
-    if not zupc:
-        abort(404)
-    form = administrative_forms.ZUPCUpdateForm(form_data={"zupc":ads.ZUPC.nom}, obj=ads)
+    form = None
+    if request.args.get("id"):
+        zupc = administrative_models.ZUPC.query.get(request.args.get("id"))
+        if not zupc:
+            abort(404)
+        form = ZUPCUpdateForm(obj=zupc)
+    else:
+        form = ZUPCreateForm()
     if request.method == "POST":
-        form.populate_obj(zupc)
-        if form.validate():
-            db.session.commit()
-            return redirect(url_for('zupc_list'))
-
+        if request.args.get("id"):
+            form.populate_obj(zupc)
+            if form.validate():
+                db.session.commit()
+                return redirect(url_for('zupc'))
+        else:
+            if form.validate():
+                zupc = administrative_models.ZUPC()
+                form.populate_obj(zupc)
+                db.session.add(zupc)
+                db.session.commit()
+                return redirect(url_for('zupc'))
     return render_template('forms/ads.html', form=form,
         form_method="POST", submit_value="Modifier")
 
@@ -57,7 +62,7 @@ def zupc_delete():
         abort(404)
     db.session.delete(zupc)
     db.session.commit()
-    return redirect(url_for("zupc_list"))
+    return redirect(url_for("zupc"))
 
 
 @mod.route('/zupc/autocomplete')