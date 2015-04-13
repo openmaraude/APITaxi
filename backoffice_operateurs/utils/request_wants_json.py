35e9450f56224f4515e51b57e6f46b1b81bac3c8
l-vincent-l <lara.vincent@gmail.com>
2015-04-13 14:31:32 +0200

    Utils: add requests wants json

---
+++ backoffice_operateurs/utils/request_wants_json.py
@@ -0,0 +1,10 @@
+#Source: http://flask.pocoo.org/snippets/45/
+
+from flask import request
+
+def request_wants_json():
+    best = request.accept_mimetypes \
+        .best_match(['application/json', 'text/html'])
+    return best == 'application/json' and \
+        request.accept_mimetypes[best] > \
+        request.accept_mimetypes['text/html']
