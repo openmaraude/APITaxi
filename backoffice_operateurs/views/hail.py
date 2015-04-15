5feb67f70e081cf26f95db7712d84d64c611b2b5
l-vincent-l <lara.vincent@gmail.com>
2015-04-15 16:47:39 +0200

    fix hail view

--- backoffice_operateurs/views/hail.py
+++ backoffice_operateurs/views/hail.py
@@ -1,52 +1,59 @@
 # -*- coding: utf8 -*-
-from flask import request, redirect, url_for
+from flask import request, redirect, url_for, abort
 from flask.ext.restplus import Resource
 from flask.ext.security import login_required, roles_required,\
         roles_accepted, current_user
-from .. import ns, db
+from .. import ns_hail, db
 from ..models import Hail as HailModel
 from datetime import datetime
 
-@ns.route('/<string:hail_id>/', endpoint='hailid')
+
 @login_required
 @roles_accepted('moteur', 'operateur')
+@ns_hail.route('/<int:hail_id>/', endpoint='hailid')
 class HailId(Resource):
 
     def get(self, hail_id):
-        hail = HailModel.query.first_or_404(hail_id)
+        hail = HailModel.query.get_or_404(hail_id)
         return hail.to_dict()
 
     def put(self, hail_id):
-        if not json:
+        json = request.get_json()
+        if not json or not 'hail' in json:
             abort(400)
-        if any(lambda f : f not in json['hail'],
-                ['client_id', 'client_lon', 'client_lat',
-                    'taxi_id', 'status']):
-            abort(400)
-        hj = json['hail']
-        hail = HailModel.get_or_404(hail_id)
         json = request.get_json(silent=True)
-        if hj['status'] not in ['received_by_taxi',
+        hj = json['hail']
+        if any(map(lambda f : f not in hj,
+                ['client_id', 'client_lon', 'client_lat',
+                    'taxi_id', 'status'])):
+            abort(400)
+        hail = HailModel.query.get_or_404(hail_id)
+        if hj['status'] != hail.status and\
+            hj['status'] not in ['received_by_taxi',
                 'accepted_by_taxi', 'declined_by_taxi',
                 'incident_taxi', 'incident_client']:
             abort(400)
         #We change the status
-        getattr(hail, hj['status'])()
+        if hasattr(hail, hj['status']):
+            getattr(hail, hj['status'])()
+        if current_user.has_role('moteur'):
+            hail.client_lon = hj['client_lon']
+            hail.client_lat = hj['client_lat']
         db.session.commit()
-        return hail.as_dict()
+        return hail.to_dict()
 
 
-@ns.route('/', endpoint='hail_endpoint')
-#@login_required
-#@roles_required('moteur')
+@login_required
+@roles_required('moteur')
+@ns_hail.route('/', endpoint='hail_endpoint')
 class Hail(Resource):
     def post(self):
-        json = get_json()
+        json = request.get_json()
         if not json:
             abort(400)
-        if any(lambda f : f not in json['hail'],
+        if any(map(lambda f : f not in json['hail'],
                 ['client_id', 'client_lon', 'client_lat',
-                    'taxi_id']):
+                    'taxi_id'])):
             abort(400)
         #@TODO: faire validation des arguments avec http://flask-restful.readthedocs.org/en/0.3.2/reqparse.html
         #@TODO: checker existence du taxi
