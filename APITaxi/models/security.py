391ff28186e40bee9ba7966b739090d67d61b2a6
l-vincent-l <lara.vincent@gmail.com>
2015-04-24 15:59:40 +0200

    Add apikey when creating a user

--- APITaxi/models/security.py
+++ APITaxi/models/security.py
@@ -1,6 +1,8 @@
 # -*- coding: utf8 -*-
 from flask.ext.security import UserMixin, RoleMixin
 from ..models import db
+from uuid import uuid4
+
 
 roles_users = db.Table('roles_users',
         db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
@@ -20,6 +22,10 @@ class User(db.Model, UserMixin):
     roles = db.relationship('Role', secondary=roles_users,
                             backref=db.backref('users', lazy='dynamic'))
     apikey = db.Column(db.String(36), nullable=False)
+
+    def __init__(self, *args, **kwargs):
+        kwargs['apikey'] = str(uuid4())
+        super(self.__class__, self).__init__(**kwargs)
 
     def get_user_from_api_key(self, apikey):
         user = self.user_model.query.filter_by(apikey=apikey)
