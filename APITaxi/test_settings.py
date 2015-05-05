409543b6a5ded3bbfacfd2cb36da66d4f3a17fd6
l-vincent-l <lara.vincent@gmail.com>
2015-05-05 18:02:05 +0200

    Add test settings

---
+++ APITaxi/test_settings.py
@@ -0,0 +1,5 @@
+DEBUG = True
+SECRET_KEY = 'super-secret'
+SQLALCHEMY_DATABASE_URI = 'postgresql://vincent:vincent@localhost/odtaxi_test'
+REDIS_URL = "redis://:@localhost:6379/0"
+REDIS_GEOINDEX = 'geoindex'
