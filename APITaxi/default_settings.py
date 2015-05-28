da5bd2427715b91a41cbb63603cd0e721f8d834f
l-vincent-l <lara.vincent@gmail.com>
2015-05-28 12:19:36 +0200

    Encrypt password

--- APITaxi/default_settings.py
+++ APITaxi/default_settings.py
@@ -3,3 +3,6 @@ SECRET_KEY = 'super-secret'
 SQLALCHEMY_DATABASE_URI = 'postgresql://v:v@localhost/odtaxi'
 REDIS_URL = "redis://:@localhost:6379/0"
 REDIS_GEOINDEX = 'geoindex'
+SECURITY_PASSWORD_HASH = 'bcrypt'
+SECURITY_PASSWORD_SALT = 'pepper'
+SECURITY_REGISTERABLE = True
