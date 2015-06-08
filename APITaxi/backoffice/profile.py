150293949ed3b48639d43202dad1600423888017
l-vincent-l <lara.vincent@gmail.com>
2015-06-08 17:21:34 +0200

    Add role adaptation and /user/<id> API

--- APITaxi/backoffice/profile.py
+++ APITaxi/backoffice/profile.py
@@ -1,17 +1,20 @@
 # -*- coding: utf-8 -*-
 from .. import db
+from ..api import api
 from ..models import security as security_models
 from ..forms.user import UserForm
 from flask.ext.security import login_required, roles_accepted, current_user
 from flask import (Blueprint, request, render_template, redirect, jsonify,
                    url_for, abort, current_app, send_file)
+from ..utils import fields
+from flask.ext.restplus import fields as basefields, marshal_with, Resource
 from werkzeug import secure_filename
 import os
 from flask_wtf import Form
 from flask_wtf.file import FileField
 import uuid
 from PIL import Image
-
+from . import ns_administrative
 
 mod = Blueprint('profile', __name__)
 @mod.route('/user/form', methods=['GET', 'POST'])
@@ -23,6 +26,18 @@ def profile_form():
         del form._fields['commercial_name']
     if not current_user.has_role('operateur'):
         del form._fields['logo']
+        del form._fields['hail_endpoint_staging']
+        del form._fields['hail_endpoint_testing']
+        del form._fields['hail_endpoint_production']
+    if current_user.has_role('prefecture'):
+        del form._fields['email_technical']
+        del form._fields['phone_number_technical']
+        form._fields['email_customer'].description = u'Adresse email de contact'
+        form._fields['email_customer'].label.text = form._fields['email_customer'].description
+        form._fields['phone_number_customer'].description = u'Numéro de téléphone de contact'
+        form._fields['phone_number_customer'].label.text = \
+                form._fields['phone_number_customer'].description
+
     if request.method == "POST" and form.validate():
         if current_user.has_role('operateur'):
             logo = form.logo
@@ -41,10 +56,41 @@ def profile_form():
         db.session.commit()
         return redirect(url_for('profile.profile_form'))
     return render_template('forms/profile.html', form=form,
-        form_method="POST", submit_value="Modifier")
+        form_method="POST", logos=current_user.logos, submit_value="Modifier",
+        )
+
+class LogoHref(basefields.Raw):
+    def output(self, key, obj):
+        return url_for('profile.image', user_id=obj.user_id, src=obj.id)
+
+model_user = api.model("user", {
+    'data': basefields.List(basefields.Nested(
+        api.model("user_model",
+            {
+                "name": fields.String(attribute='commercial_name'),
+                "logos": basefields.List(basefields.Nested(
+                    api.model('logo_model', 
+                        {'href': LogoHref,
+                         'size' : fields.String,
+                         'format': fields.String(attribute='format_'),
+                        }
+                        )
+                    ))
+            }
+        )))
+    })
+
+@ns_administrative.route('users/<int:user_id>')
+class ProfileDetail(Resource):
+    @api.marshal_with(model_user)
+    def get(self, user_id):
+        user = security_models.User.query.get(user_id)
+        if not user:
+            abort(404)
+        return {"data": [user]}, 200
 
 @mod.route('/user/<int:user_id>/images/<src>')
-def profile_images(user_id, src):
+def image(user_id, src):
     logo = security_models.Logo.query.get(src)
     if not logo:
         abort(404)
