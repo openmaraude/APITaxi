c2bebea8ce5ddf3b87f5bea0c7784115252645c8
l-vincent-l <lara.vincent@gmail.com>
2015-06-15 18:46:13 +0200

    Add documentation/index route

--- APITaxi/documentation/index.py
+++ APITaxi/documentation/index.py
@@ -5,6 +5,7 @@ from flask.ext.security import current_user
 mod = Blueprint('documentation', __name__)
 
 @mod.route('/documentation')
+@mod.route('/documentation/index')
 def doc_index():
     return render_template('documentation/index.html',
                  apikey=current_user.apikey  if current_user else 'token')
