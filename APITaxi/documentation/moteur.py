b70167c0ff2416cd5bf597927e11154bde63f57c
l-vincent-l <lara.vincent@gmail.com>
2015-06-15 15:38:51 +0200

    WIP

--- APITaxi/documentation/moteur.py
+++ APITaxi/documentation/moteur.py
@@ -5,6 +5,6 @@ from flask.ext.security import current_user
 mod = Blueprint('documentation_moteur', __name__)
 
 @mod.route('/documentation/moteur')
-def doc_index():
+def doc_moteur():
     return render_template('documentation/moteur.html',
                  apikey=current_user.apikey  if current_user else 'token')
