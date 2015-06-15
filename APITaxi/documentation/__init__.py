927073aa8df3884e4c644ba6029a7ad6e444082c
l-vincent-l <lara.vincent@gmail.com>
2015-06-15 18:46:05 +0200

    Add reference

--- APITaxi/documentation/__init__.py
+++ APITaxi/documentation/__init__.py
@@ -7,3 +7,5 @@ def init_app(app):
     app.register_blueprint(moteur.mod)
     from . import operateur
     app.register_blueprint(operateur.mod)
+    from . import reference
+    app.register_blueprint(reference.mod)
