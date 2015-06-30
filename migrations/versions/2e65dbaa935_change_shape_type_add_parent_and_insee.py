de77f283102c276dfaf280593fc81811c540c413
l-vincent-l <lara.vincent@gmail.com>
2015-06-30 14:23:35 +0200

    WIP

--- migrations/versions/2e65dbaa935_change_shape_type_add_parent_and_insee.py
+++ migrations/versions/2e65dbaa935_change_shape_type_add_parent_and_insee.py
@@ -13,16 +13,22 @@ down_revision = '4190b0aefe23'
 from alembic import op
 import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql
+import geoalchemy2 as ga
 parent_zupc = 'zupc_parent_foreign_key'
+
 def upgrade():
     op.add_column('ZUPC', sa.Column('insee', sa.String(), nullable=True))
     op.add_column('ZUPC', sa.Column('parent_id', sa.Integer(), nullable=True))
     op.create_foreign_key(parent_zupc, 'ZUPC', 'ZUPC', ['parent_id'], ['id'])
-    op.drop_constraint(u'uq_vehicle_description', 'vehicle_description', type_='unique')
-    ### end Alembic commands ###
+    op.drop_column('ZUPC', 'shape')
+    op.add_column('ZUPC', sa.Column('shape',
+        ga.Geography(geometry_type='MULTIPOLYGON', srid=4326,
+        spatial_index=False)))
 
 
 def downgrade():
     op.drop_constraint(parent_zupc, 'ZUPC', type_='foreignkey')
     op.drop_column('ZUPC', 'parent_id')
     op.drop_column('ZUPC', 'insee')
+    op.drop_column('ZUPC', 'shape')
+    op.add_column('ZUPC', sa.Column('shape', sa.String()))
