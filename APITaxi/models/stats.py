95766f6005b3deb74530f1d5b25a0ef7780f6ad3
l-vincent-l <lara.vincent@gmail.com>
2015-08-20 15:45:45 +0200

    We do not use stats model

--- APITaxi/models/stats.py
+++
@@ -1,9 +0,0 @@
-#coding: utf-8
-from ..extensions import db
-
-class ActiveTaxis(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    zupc_id = db.Column(db.Integer, index=True)
-    operator_id = db.Column(db.Integer, index=True)
-    timestamp = db.Column(db.DateTime)
-    nb_taxis = db.Column(db.Integer)
