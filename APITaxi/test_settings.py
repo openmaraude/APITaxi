73f005f55f2bbea5939f8efce4b13c1d04d29054
l-vincent-l <lara.vincent@gmail.com>
2015-10-01 16:35:34 +0200

    Add security_password_hash in tests_settings

---
+++ APITaxi/test_settings.py
@@ -0,0 +1,9 @@
+DEBUG = True
+SECRET_KEY = 'super-secret'
+SQLALCHEMY_DATABASE_URI = 'postgresql://vincent:vincent@localhost/odtaxi_test'
+REDIS_URL = "redis://:@localhost:6379/0"
+REDIS_GEOINDEX = 'geoindex'
+SQLALCHEMY_POOL_SIZE = 15
+
+
+SECURITY_PASSWORD_HASH = 'plaintext'
