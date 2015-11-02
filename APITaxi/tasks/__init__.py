d617f36698683985a8a3000b2c97df3558e6d9be
l-vincent-l <lara.vincent@gmail.com>
2015-11-02 16:22:22 +0100

    Add clean_geoindex function

--- APITaxi/tasks/__init__.py
+++ APITaxi/tasks/__init__.py
@@ -6,6 +6,7 @@ from logging import getLogger
 def init_app(app):
     celery.init_app(app)
 from .make_views import store_active_taxis as f
+from .clean_geoindex import clean_geoindex
 
 @celery.task()
 def store_active_taxis(frequency):
