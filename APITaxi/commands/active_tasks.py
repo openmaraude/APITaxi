11f21e0be6558896db0b8f981477ec8f5f43ca75
l-vincent-l <lara.vincent@gmail.com>
2015-11-24 16:00:35 +0100

    Fix active_taxis tasks

--- APITaxi/commands/active_tasks.py
+++ APITaxi/commands/active_tasks.py
@@ -4,6 +4,8 @@ from ..extensions import celery
 @manager.command
 def active_tasks(queue):
     i = celery.control.inspect()
+    if i.active() is None:
+        return ''
     for tasks in i.active().values():
         for t in tasks:
             if t['delivery_info']['routing_key'] == queue:
