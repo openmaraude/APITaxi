af4c057a839515ea5c15ddd9710ed651ff35ba46
odtvince <github@open-data.org>
2015-11-30 12:35:56 +0100

    Fix the content negotiation for MSIE

--- APITaxi/utils/request_wants_json.py
+++ APITaxi/utils/request_wants_json.py
@@ -8,7 +8,7 @@ def request_wants_json():
     best = request.accept_mimetypes \
         .best_match(['application/json', 'text/html'])
     return best == 'application/json' and \
-        request.accept_mimetypes[best] >= \
+        request.accept_mimetypes[best] > \
         request.accept_mimetypes['text/html']
 
 
