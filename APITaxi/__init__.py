4d8611c9d98f414fa6be624627b5f9d7fac29e8b
l-vincent-l <lara.vincent@gmail.com>
2016-01-08 17:04:04 +0100

    Warm-up the cache at startup

--- APITaxi/__init__.py
+++ APITaxi/__init__.py
@@ -90,4 +90,11 @@ def create_app(sqlalchemy_uri=None):
 
     from .models import security
     user_datastore.init_app(db, security.User, security.Role)
+    def warm_up_redis():
+        from .models.taxi import Taxi as TaxiModel
+        for taxi in TaxiModel.query.all():
+            for description in taxi.vehicle.descriptions:
+                TaxiModel.set_redis_status(description)
+    app.warm_up_redis = warm_up_redis
+
     return app
