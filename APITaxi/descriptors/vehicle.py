25d1542688f18fec3d23870c94d0dbed121d6fa4
l-vincent-l <lara.vincent@gmail.com>
2016-02-04 17:33:05 +0100

    Remove make_model function

---
+++ APITaxi/descriptors/vehicle.py
@@ -0,0 +1,18 @@
+#coding: utf-8
+from ..api import api
+from ..models.vehicle import Vehicle
+from APITaxi_utils import fields
+
+vehicle_model = api.model('vehicle_model_data',
+    {'data': fields.List(fields.Nested(
+        api.model('vehicle_model', Vehicle.marshall_obj())
+        ))
+    }
+)
+
+vehicle_expect = api.model('vehicle_expect_data',
+    {'data': fields.List(fields.Nested(
+        api.model('vehicle_expect', Vehicle.marshall_obj(filter_id=True))
+        ))
+    }
+)
