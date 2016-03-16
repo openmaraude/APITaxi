f4c5ec240652c2cf9ebec4cd1fbb3f775958cdad
l-vincent-l <lara.vincent@gmail.com>
2016-03-16 17:23:20 +0100

    Adapt to new version

--- APITaxi/tasks/make_views.py
+++ APITaxi/tasks/make_views.py
@@ -1,7 +1,7 @@
 #coding: utf-8
 from ..extensions import redis_store, user_datastore
-from APITaxi.models.taxis import Taxi
-from APITaxi.models.administrative import ZUPC
+from APITaxi_models.taxis import Taxi
+from APITaxi_models.administrative import ZUPC
 from itertools import izip, ifilter, imap
 from datetime import datetime, timedelta
 from time import mktime, time
