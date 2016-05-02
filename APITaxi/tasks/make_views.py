0c3f0506509932fee103dc3422b65bd178a479b4
l-vincent-l <lara.vincent@gmail.com>
2016-05-02 16:03:02 +0200

    Send new measurement with only available taxis to influxdb

--- APITaxi/tasks/make_views.py
+++ APITaxi/tasks/make_views.py
@@ -7,6 +7,7 @@ from datetime import datetime, timedelta
 from time import mktime, time
 from flask import current_app
 from APITaxi_utils import influx_db
+from itertools import izip_longest, compress
 
 def store_active_taxis(frequency):
     now = datetime.utcnow()
@@ -30,6 +31,7 @@ def store_active_taxis(frequency):
         if taxi_db.ads is None:
             current_app.logger.error('Taxi: {} is invalid'.format(taxi_id))
             continue
+#This a cache for insee to zupc.
         if not taxi_db.ads.insee in insee_zupc_dict:
             zupc = ZUPC.query.get(taxi_db.ads.zupc_id)
             if not zupc:
@@ -40,27 +42,46 @@ def store_active_taxis(frequency):
             insee_zupc_dict[taxi_db.ads.insee] = zupc
         else:
             zupc = insee_zupc_dict[taxi_db.ads.insee]
-        if zupc.insee not in map_insee_nb_active:
-            map_insee_nb_active[zupc.insee] = set()
-        map_insee_nb_active[zupc.insee].add(taxi_id)
+        map_insee_nb_active.setdefault(zupc.insee, set()).add(taxi_id)
         if operator not in map_operateur_insee_nb_active:
             u = user_datastore.find_user(email=operator)
             if not u:
                 current_app.logger.error('User: {} not found'.format(operator))
                 continue
             map_operateur_insee_nb_active[operator] = dict()
-        if zupc.insee not in map_operateur_insee_nb_active[operator]:
-            map_operateur_insee_nb_active[operator][zupc.insee] = set()
-        map_operateur_insee_nb_active[operator][zupc.insee].add(taxi_id)
-        if operator not in map_operateur_nb_active:
-            map_operateur_nb_active[operator] = set()
-        map_operateur_nb_active[operator].add(taxi_id)
+        map_operateur_insee_nb_active[operator].setdefault(zupc.insee, set()).add(taxi_id)
+        map_operateur_nb_active.setdefault(operator, set()).add(taxi_id)
+
+    map_operateur_insee_nb_available = dict()
+    map_operateur_nb_available = dict()
+
+    available_ids = set()
+    for operator, insee_taxi_ids in map_operateur_insee_nb_active.iteritems():
+        for insee, taxis_ids in insee_taxi_ids.iteritems():
+            for ids in izip_longest(*[taxis_ids]*100, fillvalue=None):
+                pipe = redis_store.pipeline()
+                for id_ in ids_:
+                    pipe.sismember(current_app.config['REDIS_TIMESTAMPS'],
+                                   i+':'+operator)
+                res = compress(ids, pipe.execute())
+                map_operateur_insee_nb_available(operator, dict()).setdefault(
+                    insee, set()).union(set(res))
+                available_ids.union(set(res))
+
+    for operateur, taxis_ids in map_operateur_nb_active.iteritems():
+        map_operateur_nb_active.setdefault(operateur, set()).union(
+            set([t+':'+operateur for t in taxis_ids]).intersection(avaliable_ids
+                )
+        )
+
 
     client = influx_db.get_client(current_app.config['INFLUXDB_TAXIS_DB'])
     to_insert = []
     bucket_size = 100
-    measurement_name = "nb_taxis_every_{}".format(frequency)
-    def insert(operator, zupc, active):
+    def insert(operator, zupc, active, available=False):
+        measurement_name = "nb_taxis_every_{}".format(frequency)
+        if available:
+            measurement_name += '_available'
         to_insert.append({
                     "measurement": measurement_name,
                     "tags": {
@@ -89,6 +110,13 @@ def store_active_taxis(frequency):
         insert(operateur, "", active)
 
     insert("", "", active_taxis)
+
+    for operator, zupc_available in map_operateur_insee_nb_active.iteritems():
+        for zupc, available in zupc_active.iteritems():
+            insert(operator, zupc, available, True)
+
+    for zupc, available in map_insee_nb_active.iteritems():
+        insert("", zupc, available, True)
 
     if len(to_insert) > 0:
         current_app.logger.debug('To insert: {}'.format(to_insert))
