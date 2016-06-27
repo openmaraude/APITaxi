76e7ec6c4790528fcd5057808551e0898453c490
l-vincent-l <lara.vincent@gmail.com>
2016-06-27 12:09:53 +0200

    Add GET /users/ endpoint

--- APITaxi/api/__init__.py
+++ APITaxi/api/__init__.py
@@ -11,7 +11,7 @@ ns_administrative = api.namespace('administrative',
         description="Administrative APIs", path='/')
 
 def init_app(app):
-    from . import hail, taxi, ads, drivers, zupc, profile, vehicle, documents
+    from . import hail, taxi, ads, drivers, zupc, profile, vehicle, documents, users
     api.init_app(app, add_specs=False)
     app.register_blueprint(api_blueprint)
     app.register_blueprint(apidoc.apidoc)
---
+++ tests/users_tests.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from .skeleton import Skeleton
+
+class TestUsersGet(Skeleton):
+    url = '/users/'
+    def test_get_admin_user(self):
+        r = self.get(role='admin')
+        self.assert200(r)
+        data = r.json['data']
+        assert len(data) == 7
+        assert all(map(lambda d: 'apikey' in d and 'name' in d, data))
+
+    def test_get_operateur_user(self):
+        r = self.get(role='operateur')
+        assert r.status_code == 302
+
+    def test_get_moteur_user(self):
+        r = self.get(role='moteur')
+        assert r.status_code == 302
+
+    def test_get_no_login(self):
+        r = self.get(headers={"X-API-KEY": "aa",
+                              "Accept": "application/json",
+                              "X-VERSION": 2})
+        assert r.status_code == 302
