d88f3046e95c472309437a6984f22972b34e6196
l-vincent-l <lara.vincent@gmail.com>
2016-11-25 12:23:59 +0100

    Use json instead of pickle in celery

--- APITaxi/default_settings.py
+++ APITaxi/default_settings.py
@@ -38,8 +38,8 @@ DOGPILE_CACHE_URLS = ['redis://localhost:6379/0']
 SQLALCHEMY_POOL_SIZE = 15
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
-CELERY_ACCEPT_CONTENT = ['pickle']
-CELERY_TASK_SERIALIZER = 'pickle'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
 CELERYBEAT_SCHEDULE_FILENAME  = '/tmp/celery_beat_schedule'
 
 from celery.schedules import crontab
