7b3c35a3e6c59ff860ad02c6eac23f6a6b4ebbf1
Julien Castets <castets.j@gmail.com>
2019-10-18 22:31:44 +0200

    APITaxi/__init__.py: PEP8

--- APITaxi/__init__.py
+++ APITaxi/__init__.py
@@ -1,71 +1,74 @@
 # -*- coding: utf-8 -*-
 
+import os
+
+from flask import Flask, g, request_started, request_finished
+from flask_uploads import configure_uploads
+
+from APITaxi_models import db, security, HailLog
+from APITaxi_utils.login_manager import init_app as init_login_manager
+from APITaxi_utils.version import check_version, add_version_header
+
+from . import api, tasks
+from .api.extensions import documents
+from .commands.warm_up_redis import warm_up_redis_func
+from .extensions import redis_store, redis_store_saved, user_datastore
+
+
 __author__ = 'Vincent Lara'
 __contact__ = "vincent.lara@data.gouv.fr"
 __homepage__ = "https://github.com/"
 __version__ = '0.1.0'
 __doc__ = 'Flask application to serve APITaxi'
 
-from flask import Flask, request_started, request_finished
-import os
 
 def create_app():
-    from .extensions import redis_store, redis_store_saved, user_datastore
     app = Flask(__name__)
     app.config.from_object('APITaxi.default_settings')
     if 'APITAXI_CONFIG_FILE' in os.environ:
         app.config.from_envvar('APITAXI_CONFIG_FILE')
-    if not 'ENV' in app.config:
+    if 'ENV' not in app.config:
         app.logger.error('ENV is needed in the configuration')
         return None
     if app.config['ENV'] not in ('PROD', 'STAGING', 'DEV'):
         app.logger.error("""Here are the possible values for conf['ENV']:
         ('PROD', 'STAGING', 'DEV') your's is: {}""".format(app.config['env']))
         return None
-    #Load configuration from environment variables
+    # Load configuration from environment variables
     for k in list(app.config.keys()):
-        if not k in os.environ:
+        if k not in os.environ:
             continue
         app.config[k] = os.environ[k]
 
-    from APITaxi_models import db, security, HailLog
     db.init_app(app)
     redis_store.init_app(app)
     redis_store_saved.init_app(app)
-    from . import api
     api.init_app(app)
 
-    from APITaxi_utils.version import check_version, add_version_header
     request_started.connect(check_version, app)
     request_finished.connect(add_version_header, app)
 
-    from flask_uploads import configure_uploads
-    from .api.extensions import documents
     configure_uploads(app, (documents,))
-    from APITaxi_utils.login_manager import init_app as init_login_manager
     init_login_manager(app, user_datastore, None)
 
-    from . import tasks
     tasks.init_app(app)
 
     user_datastore.init_app(db, security.User, security.Role)
 
     @app.before_first_request
     def warm_up_redis():
-        from APITaxi.commands.warm_up_redis import warm_up_redis_func
         warm_up_redis_func(app, db, security.User, redis_store)
 
     def delete_redis_keys(response):
-        from flask import g
         if not hasattr(g, 'keys_to_delete'):
             return response
         redis_store.delete(*g.keys_to_delete)
         return response
 
     app.after_request_funcs.setdefault(None, []).append(
-            HailLog.after_request(redis_store_saved)
+        HailLog.after_request(redis_store_saved)
     )
     app.after_request_funcs.setdefault(None, []).append(
-            delete_redis_keys
+        delete_redis_keys
     )
     return app
