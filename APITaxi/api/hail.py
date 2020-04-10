6c9b736f4eb3aade7615b7f014fcb601cb7ca4fd
Julien Castets <castets.j@gmail.com>
2020-04-10 12:29:00 +0200

    Always save taxi phone number on PUT /hails/:id
    When PUT /hails/:id is called with the status "accepted_by_taxi", the
    taxi's phone number must be provided.
    
    Before, it was only saved for X-Version == 2. Now, it is saved for any
    version >=2.
    
    I prefer not to enforce the parameter for any version (< 2), as it would
    break the backward compatibility, and I don't know if all clients
    provide the X-Version header.

--- APITaxi/api/hail.py
+++ APITaxi/api/hail.py
@@ -73,7 +73,7 @@ class HailId(Resource):
 
         #We change the status
         if 'status' in hj and  hj['status'] == 'accepted_by_taxi':
-            if g.version == 2:
+            if g.version >= 2:
                 if not 'taxi_phone_number' in hj or hj['taxi_phone_number'] == '':
                     abort(400, message='Taxi phone number is needed')
                 else:
