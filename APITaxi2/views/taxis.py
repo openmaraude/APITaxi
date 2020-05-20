dbf6a420cc3238946a188392a2ee3f347f3895f0
Julien Castets <castets.j@gmail.com>
2020-05-20 15:50:58 +0200

    GET /taxis/:id: refactoring
    Replace custom marshmallow fields and with a simpler post_dump
    processor.

--- APITaxi2/views/taxis.py
+++ APITaxi2/views/taxis.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from flask_security import current_user, login_required, roles_accepted
 
-from marshmallow import fields, Schema
+from marshmallow import decorators, fields, Schema
 
 from sqlalchemy.orm import joinedload
 
@@ -18,62 +18,12 @@ from ..validators import (
 blueprint = Blueprint('taxis', __name__)
 
 
-def _get_vehicle_desc(vehicle, operator, fields):
-    """A Vehicle can be registered by several operators. This function
-    retrieves the vehicle description for `operator`, and returns the attribute
-    as specified by `fields`.
-
-    For example the call:
-
-    >>> _get_vehicle_desc(vehicle, operator, 'model.name')
-
-    retrieves the vehicle description registered by "operator", and returns the
-    `name` of the `model` attribute.
-    """
-    for description in vehicle.descriptions:
-        if description.added_by == operator:
-            break
-    else:
-        raise AssertionError('No VehicleDescription of vehicle %s for operator %s' % (vehicle, operator))
-
-    ret = description
-    for part in fields.split('.'):
-        if not ret:
-            return None
-        ret = getattr(ret, part)
-    return ret
-
-
 def taxis_details_schema(taxi, taxi_operator):
     """A taxi can be registered with several operators. This function returns
     the Schema to display `taxi` registered with `operator`."""
+
+    # Get taxi's location stored in redis.
     taxi_redis = redis_backend.get_taxi(taxi.id, taxi.added_by.email)
-
-    class PowerSchema(Schema):
-        def get_attribute(self, obj, attr, default):
-            """Extend Schema to accept when fields.attribute is a lambda.
-
-            See https://github.com/marshmallow-code/marshmallow/issues/1591
-            """
-            if callable(attr):
-                return attr(obj)
-            return super().get_attribute(obj, attr, default)
-
-    class VehicleSchema(PowerSchema):
-        model = fields.String(
-            attribute=lambda vehicle: _get_vehicle_desc(vehicle, taxi_operator, 'model.name')
-        )
-        constructor = fields.String(
-            attribute=lambda vehicle: _get_vehicle_desc(vehicle, taxi_operator, 'constructor.name')
-        )
-        color = fields.String(
-            attribute=lambda vehicle: _get_vehicle_desc(vehicle, taxi_operator, 'color')
-        )
-        licence_plate = fields.String()
-
-        nb_seats = fields.String(
-            attribute=lambda vehicle: _get_vehicle_desc(vehicle, taxi_operator, 'nb_seats')
-        )
 
     class ADSSchema(Schema):
         numero = fields.String()
@@ -83,29 +33,18 @@ def taxis_details_schema(taxi, taxi_operator):
         professional_licence = fields.String()
         departement = fields.String(attribute='departement.numero')
 
-    class TaxiSchema(PowerSchema):
+    class TaxiSchema(Schema):
         id = fields.String()
-        internal_id = fields.String(
-            attribute=lambda taxi: _get_vehicle_desc(taxi.vehicle, taxi_operator, 'internal_id')
-        )
         operator = fields.Constant(taxi_operator.email)
-        vehicle = fields.Nested(VehicleSchema)
         ads = fields.Nested(ADSSchema)
         driver = fields.Nested(DriverSchema)
-        characteristics = fields.List(
-            fields.String,
-            attribute=lambda taxi: _get_vehicle_desc(taxi.vehicle, taxi_operator, 'characteristics')
-        )
         rating = fields.Float()
 
-        status = fields.String(
-            attribute=lambda taxi: _get_vehicle_desc(taxi.vehicle, taxi_operator, 'status')
-        )
         last_update = fields.Constant(taxi_redis.timestamp if taxi_redis else None)
 
         position = fields.Method('_position')
 
-        def _position(self, taxi):
+        def _position(self, _taxi):
             lon = taxi_redis.lon if taxi_redis else None
             lat = taxi_redis.lat if taxi_redis else None
             return {'lon': lon, 'lat': lat}
@@ -114,6 +53,49 @@ def taxis_details_schema(taxi, taxi_operator):
         # the location of the caller. This field is returned only for backward
         # compatibility purpose.
         crowfly_distance = fields.Constant(None)
+
+        @decorators.post_dump(pass_original=True)
+        def add_vehicle_description_fields(self, data, taxi, many=False):
+            """The Vehicle object linked to this Taxi may have several
+            descriptions (one per operator) one of which being the one for the
+            operator making the request.
+
+            It is not easily possible to write marshmallow fields to access the
+            correct description object.
+
+            This function finds the operator's VehicleDescription object, and
+            update the serialized object with fields from this object.
+
+            See: https://github.com/marshmallow-code/marshmallow/issues/1591
+            """
+            for vehicle_description in taxi.vehicle.descriptions:
+                if vehicle_description.added_by == taxi_operator:
+                    break
+            else:
+                # At this point the taxi should have a description for the operator making
+                # the request. If no description exists, taxis_details view
+                # should have raised HTTP/404.
+                raise AssertionError(
+                    'No VehicleDescription of vehicle %s for operator %s' % (
+                        taxi.vehicle, taxi_operator
+                    )
+                )
+
+            data.update({
+                'internal_id': vehicle_description.internal_id,
+                'characteristics': vehicle_description.characteristics,
+                'status': vehicle_description.status,
+                'vehicle': {
+                    'model': vehicle_description.model.name
+                        if vehicle_description.model else None,
+                    'constructor': vehicle_description.constructor.name
+                        if vehicle_description.constructor else None,
+                    'color': vehicle_description.color,
+                    'licence_plate': taxi.vehicle.licence_plate,
+                    'nb_seats': vehicle_description.nb_seats,
+                }
+            })
+            return data
 
     return data_schema_wrapper(TaxiSchema)
 
