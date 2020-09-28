45bed712a136f86c01182a32b964139cd34bac1f
Julien Castets <castets.j@gmail.com>
2020-09-28 17:25:47 +0200

    Move send_request_operator.py to operators.py

--- APITaxi2/tasks/__init__.py
+++ APITaxi2/tasks/__init__.py
@@ -3,4 +3,4 @@ from celery import Celery
 celery = Celery(__name__)
 
 from .clean import clean_geoindex_timestamps  # noqa
-from .send_request_operator import send_request_operator  # noqa
+from .operators import send_request_operator  # noqa
