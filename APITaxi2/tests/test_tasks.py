94e2de80f804401305c7266e22edc2ee39977bf2
Julien Castets <castets.j@gmail.com>
2020-10-16 08:21:59 +0200

    Add unittests for tasks.handle_hail_timeout

--- APITaxi2/tests/test_tasks.py
+++ APITaxi2/tests/test_tasks.py
@@ -1,6 +1,10 @@
 import time
+from unittest import mock
 
-from APITaxi2 import tasks
+from APITaxi_models2 import Hail, VehicleDescription
+from APITaxi_models2.unittest.factories import HailFactory, VehicleDescriptionFactory
+
+from .. import tasks
 
 
 class TestCleanGeoindexTimestamps:
@@ -27,3 +31,40 @@ class TestCleanGeoindexTimestamps:
         assert len(app.redis.zrange('geoindex', 0, -1)) == 1
         assert len(app.redis.zrange('timestamps', 0, -1)) == 1
         assert len(app.redis.zrange('geoindex_2', 0, -1)) == 1
+
+
+class TestHandleHailTimeout:
+    def test_timeout(self, app):
+        """Hail reaches timeout. Status is initially sent_to_operator, and it
+        still has the same status when timeout function is called. New taxi
+        status is 'off'."""
+        hail = HailFactory(status='sent_to_operator')
+        hail_id = hail.id
+
+        # Hail status timeout: it is still sent_to_operator
+        with mock.patch.object(tasks.operators.current_app.logger, 'warning') as mocked_logger:
+            tasks.handle_hail_timeout(hail.id, hail.operateur.id, 'sent_to_operator', 'failure', 'off')
+            assert mocked_logger.call_count == 1
+
+        # Since handle_hail_timeout commits the session, we have to fetch Hail
+        # back to read its properties.
+        hail = Hail.query.get(hail_id)
+        assert hail.status == 'failure'
+
+        vehicle_description = VehicleDescription.query.one()
+        assert vehicle_description.status == 'off'
+
+    def test_no_timeout(self, app):
+        """Hail doesn't reach timeout. Status is initially
+        received_by_operator, and it is sent_to_operator when timeout function
+        is called."""
+        hail = HailFactory(status='sent_to_operator')
+        VehicleDescriptionFactory(vehicle=hail.taxi.vehicle)
+
+        hail.status = 'received_by_operator'
+        with mock.patch.object(tasks.operators.current_app.logger, 'warning') as mocked_logger:
+            tasks.handle_hail_timeout(hail.id, hail.operateur.id, 'sent_to_operator', 'failure', None)
+            assert mocked_logger.call_count == 0
+
+        # No need to fetch back hail since session has not been committed.
+        assert hail.status == 'received_by_operator'
