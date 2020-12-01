5a8c1bf9c7051af2ecc3e52333c2e2e9b75b822f
Julien Castets <castets.j@gmail.com>
2020-12-01 17:21:03 +0100

    Rework hails command
    * Log with print instead of logger.info
    * Improve performances and reduce number of SQL queries
    * Fix: never update hail with session_id = NULL

--- APITaxi2/commands/hails.py
+++ APITaxi2/commands/hails.py
@@ -2,6 +2,7 @@ import datetime
 import uuid
 
 from flask import Blueprint, current_app
+from sqlalchemy import func
 
 from APITaxi_models2 import db, Hail
 
@@ -20,41 +21,33 @@ def compute_session_ids():
     sessions = {}
 
     # Also include hails already assigned a session ID to rebuild the history
-    for i, (hail_id, session_id, last_status_change, customer_id, added_by_id) in enumerate(db.session.query(
-        Hail.id, Hail.session_id, Hail.last_status_change, Hail.customer_id, Hail.added_by_id
-    ).order_by(
-        Hail.last_status_change
-    )):
+    for i, hail in enumerate(db.session.query(Hail).order_by(Hail.last_status_change)):
         # Customer IDs are unique to the search engine were they are registered
-        key = (customer_id, added_by_id)
+        key = (hail.customer_id, hail.added_by_id)
 
         # If this hail was already assigned a session ID, just record it
-        if session_id:
-            sessions[key] = (session_id, last_status_change)
+        if hail.session_id:
+            sessions[key] = (hail.session_id, hail.last_status_change)
             continue
 
-        session_id = None
+        new_session_id = func.uuid_generate_v4()
 
         # If there is already one session for this customer which has been
         # updated less than 5 minutes ago, reuse the same session id.
         # Otherwise, generate a new one.
         if key in sessions:
             previous_session_id, previous_status_change = sessions[key]
-            if previous_status_change >= last_status_change - datetime.timedelta(minutes=5):
-                session_id = previous_session_id
-            else:
-                session_id = uuid.uuid4()
+            if previous_status_change >= hail.last_status_change - datetime.timedelta(minutes=5):
+                new_session_id = previous_session_id
 
-        db.session.query(Hail).filter(
-            Hail.id == hail_id
-        ).update(
-            {'session_id': session_id}
-        )
-        db.session.commit()
+        hail.session_id = new_session_id
 
         # Script feedback
         if not i % 1000:
-            current_app.logger.info("Hails reviewed up to %s", last_status_change)
+            print("Hails reviewed up to %s", hail.last_status_change)
+            db.session.flush()
 
         # and keep it for the next round
-        sessions[key] = (session_id, last_status_change)
+        sessions[key] = (new_session_id, hail.last_status_change)
+
+    db.session.commit()
