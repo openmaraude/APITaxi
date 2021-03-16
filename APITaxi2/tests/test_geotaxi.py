c9a337bf10220283e03a0bb52abe2f665618b5b1
Hervé Cauwelier <herve.cauwelier@beta.gouv.fr>
2021-03-16 10:41:57 +0100

    geotaxi: backport redis tests from the worker

--- APITaxi2/tests/test_geotaxi.py
+++ APITaxi2/tests/test_geotaxi.py
@@ -27,7 +27,7 @@ class TestPostPositions:
         redis_taxi = redis_backend.get_taxi(taxi.id, taxi.added_by.email)
         assert redis_taxi is None
 
-    def test_ok(self, operateur):
+    def test_ok(self, operateur, app):
         taxi = factories.TaxiFactory(added_by=operateur.user)
         response = operateur.client.post('/geotaxi', json={
             'data': [{
@@ -36,6 +36,25 @@ class TestPostPositions:
         })
         assert response.status_code == 200
 
+        # There should be five keys stored (after the IP index was dropped)
+        taxi_key = taxi.id.encode()
+        operator_key = operateur.user.email.encode()
+        taxi_operator_key = b'%s:%s' % (taxi_key, operator_key)
+        assert set(app.redis.keys()) == {
+            b'taxi:%s' % taxi_key,
+            b'geoindex',
+            b'geoindex_2',
+            b'timestamps',
+            b'timestamps_id',
+        }
+
+        assert operator_key in app.redis.hgetall(b'taxi:%s' % taxi_key)
+        assert app.redis.geohash(b'geoindex', taxi_key) == ['u09tvqxnnu0']
+        assert app.redis.geohash(b'geoindex_2', taxi_operator_key) == ['u09tvqxnnu0']
+        assert app.redis.zrange(b'timestamps', 0, -1) == [taxi_operator_key]
+        assert app.redis.zrange(b'timestamps_id', 0, -1) == [taxi_key]
+
+        # High-level API
         redis_taxi = redis_backend.get_taxi(taxi.id, operateur.user.email)
         assert redis_taxi
         assert redis_taxi.lon == 2.35 and redis_taxi.lat == 48.86
