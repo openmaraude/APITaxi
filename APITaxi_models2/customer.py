da4b390a7b0c65ee492b91868c8c7dccd38d6e27
Hervé Cauwelier <herve.cauwelier@beta.gouv.fr>
2022-07-27 14:16:18 +0200

    models: comment so I don't get bitten again

--- APITaxi_models2/customer.py
+++ APITaxi_models2/customer.py
@@ -7,8 +7,10 @@ class Customer(HistoryMixin, db.Model):
     def __repr__(self):
         return '<Customer %s (added_by id %s)>' % (self.id, self.added_by_id)
 
+    # Customer IDs are unique per moteur
     id = db.Column(db.String, primary_key=True)
     added_by_id = db.Column('added_by', db.Integer, db.ForeignKey('user.id'), primary_key=True)
+
     ban_begin = db.Column(db.DateTime)
     ban_end = db.Column(db.DateTime)
     phone_number = db.Column(db.String, nullable=False)
