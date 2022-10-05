58f77ffdf268701183bb6b248ed755436c2612df
Hervé Cauwelier <herve.cauwelier@beta.gouv.fr>
2022-10-05 18:13:21 +0200

    tests: stupid me

--- APITaxi2/tests/test_stats.py
+++ APITaxi2/tests/test_stats.py
@@ -1,9 +1,14 @@
+from APITaxi_models2.unittest import factories
+
+
 class TestStats:
 
     def test_stats_taxis(self, admin):
+        factories.VehicleDescriptionFactory()
+
         resp = admin.client.get('/stats/taxis')
         assert resp.status_code == 200
 
-    def test_stats_taxis(self, admin):
+    def test_stats_hails(self, admin):
         resp = admin.client.get('/stats/hails')
         assert resp.status_code == 200
